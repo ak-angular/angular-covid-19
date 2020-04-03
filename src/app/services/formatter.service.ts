ba4979f4c32452b0613857758ac907e713795d21
Ashish <ashish.kumar@tadigital.com>
2020-04-03 21:02:38 +0530

    added comments to services

--- src/app/services/formatter.service.ts
+++ src/app/services/formatter.service.ts
@@ -8,16 +8,34 @@ export class FormatterService {
 
   constructor() { }
 
+  /**
+   * @method
+   * @desc formats number in US metrics comma based. example 9876543 => 9,876,543
+   * @param num {Number} number to be formated
+   * @return {String} formated number in string format
+   */
   number(num: number) {
     return num && num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
   }
 
+  /**
+   * @method
+   * @desc formated date string from now. example: 5 minutes ago, 3 days agon
+   * @param dateString {String} date string to be formated
+   * @return {string} formated date string
+   */
   date(dateString: string) {
     let myMoment: moment.Moment = moment(dateString);
 
     return myMoment.fromNow();
   }
 
+  /**
+   * @method
+   * @desc parametries the object in query-string format. example: {a: 1, b: 2} => 'a=1&b=2'
+   * @param queryObj {object} object to be parametries
+   * @return {string} query string
+   */
   param(queryObj): string {
     let queryString = [];
 
--- src/app/services/http.service.ts
+++ src/app/services/http.service.ts
@@ -8,6 +8,13 @@ export class HttpService {
 
   constructor(private http: HttpClient) { }
 
+  /**
+   * @method
+   * @desc makes HTTP get call
+   * @param url {String} url where ajax call has to be made
+   * @param config {obejct} other configuration like headers etc...
+   * @return {Promise} HTTP promise object
+   */
   get(url, config = {}) {
     return this.http.get(url, config);
   }
