148f30b79f97dcaa5807de9867c35168863b081b
Ashish <ashish.kumar@tadigital.com>
2020-04-10 16:34:50 +0530

    Updated footer

--- src/app/common/footer/footer.component.ts
+++ src/app/common/footer/footer.component.ts
@@ -6,6 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
+  year = new Date().getFullYear();
+  author = {
+    name: 'Ashish Kumar',
+    url: 'https://lab.kumarashish.com/'
+  };
 
   constructor() { }
 
